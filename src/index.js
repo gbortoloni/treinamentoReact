9595bc58c1b7614c7349e3b3c18626a28802c915
Giuliano Bortoloni <giulianoagria@hotmail.com>
2019-04-30 21:25:58 -0300

    subindo exercicios

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+// import PrimeiroComponente from "./componentes/PrimeiroComponente";
+// import { CompA, CompB } from "./componentes/DoisComponentes";
+import MultiElementos from "./componentes/MultiElementos";
+const elemento = document.getElementById("root");
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+  <div>
+    <MultiElementos />
+    {/* <CompA valor="Ola eu sou o A" />
+    <CompB valor="Ola eu sou o B" /> */}
+    {/* <PrimeiroComponente valor="Ola"/> */}
+  </div>,
+  elemento
+);
